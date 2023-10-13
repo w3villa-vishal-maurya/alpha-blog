c26256e42e18b827e86a880b9857deb300c6c494
W3villa <vishalprakash.meaurya@w3villa.com>
2023-10-13 18:53:45 +0530

    add ui~

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root "articles#index"
+  root "pages#home"
 end
